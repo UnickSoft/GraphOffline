911a2b25eb19f281b9b38d08043b91e7529c57d2
oleg <mail4@felenasoft.com>
2015-12-03 20:47:49 +0300

    Fix Windows

--- test/runTestCGI.bat
+++ test/runTestCGI.bat
@@ -1,20 +1,11 @@
 @echo off
 
-set exePath="../Debug/GraphOffline.exe"
-
-rem FOR /F "tokens=1-3" %%A IN (testList.txt) DO (
-rem 	set REQUEST_METHOD="POST"
-rem 	set CONTENT_LENGTH=312
-rem 	set QUERY_STRING=%%Bto%%C
-rem 	type %%A | %exePath%
-rem )
-
+set exePath="../bin/Windows/Release/GraphOffline.exe"
 
  	set REQUEST_METHOD="POST"
  	set CONTENT_LENGTH=1587
  	set QUERY_STRING=n0ton10
  	type graph_shortPath_3.xml | %exePath%
-
 
 @echo OK
 exit
--- test/runTests.bat
+++ test/runTests.bat
@@ -1,12 +1,12 @@
 @echo off
 
-set exePath="../Debug/GraphOffline.exe"
+set exePath="../bin/Windows/Release/GraphOffline.exe"
 
-FOR /F "tokens=1-3" %%A IN (testList.txt) DO (
-	%exePath% -sp %%A -start %%B -finish %%C > %%A.test
-	fc %%A.test %%A.res > nul
-	if errorlevel 1 @echo %%A && goto faild
-	del %%A.test
+FOR /F "tokens=1-4" %%A IN (testList.txt) DO (
+	%exePath% %%A %%B -start %%C -finish %%D > %%B.test
+	fc %%B.test %%B.res > nul
+	if errorlevel 1 @echo %%B && goto faild
+	del %%B.test
 )
 
 @echo OK
