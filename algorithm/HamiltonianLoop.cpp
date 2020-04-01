76e77ea130177d78da31f6766f28beb6080a9cae
Unick Soft <admin@unick-soft.ru>
2020-04-01 22:12:58 +0200

    Fix build break

--- algorithm/HamiltonianLoop.cpp
+++ algorithm/HamiltonianLoop.cpp
@@ -14,7 +14,7 @@
 #include "Logger.h"
 #include <vector>
 #include <unordered_set>
-#include <assert.h>
+#include <cassert>
 
 static const char* g_indexStr = "index";
 static const char* g_indexCountStr = "indexCount";
